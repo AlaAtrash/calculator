955fa767767ba9f1df0fe6d429f97abf68197b8f
Ophélie <ophelie_thomas@hotmail.com>
2023-01-15 11:59:08 +0100

    rebase

---
+++ dist/multiplication.js
@@ -0,0 +1,14 @@
+"use strict";
+let inputA = document.querySelector('#numberA');
+let numberA = inputA.value;
+let inputB = document.querySelector('#numberB');
+let numberB = inputB.value;
+let button = document.querySelector('.button-multiplication');
+let resultat;
+function multiplication(a, b) {
+    resultat = a * b;
+    return resultat;
+}
+;
+button.addEventListener('click', multiplication(numberA, numberB), alert("Le résultat de votre multiplication est " + resultat));
+// button.addEventListener('click', multiplication(numberA, numberB))
