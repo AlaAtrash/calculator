f1b8e4207687b34b21571f1d227298c9afe9dd88
Ophélie <ophelie_thomas@hotmail.com>
2023-01-17 19:54:46 +0100

    test ts file

---
+++ src/multiplication.ts
@@ -0,0 +1,14 @@
+let inputA = document.querySelector('#numberA') as HTMLInputElement;
+let numberA = inputA.value;
+let inputB = document.querySelector('#numberB') as HTMLInputElement;
+let numberB = inputB.value;
+let button = document.querySelector('.button-multiplication') as HTMLButtonElement;
+let resultat: number
+
+
+function multiplication(a: number, b: number): number {
+        resultat = a * b
+        return resultat       
+};
+
+button.addEventListener('click', multiplication(numberA, numberB), alert("Le résultat de votre multiplication est " + resultat))
