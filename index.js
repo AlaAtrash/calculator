173202e77851fb00efa551d7f428d38babe16527
Abdel Bailoul <ab.bailoul@gmail.com>
2023-01-21 17:24:44 +0100

    first commit

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-import * as fs from "fs";
-import { execSync } from "child_process";
-import * as path from "path";
+import * as fs from 'fs';
+import { execSync } from 'child_process';
+import * as path from 'path';
 
 // example file and directory to copy
 //const sourceDirectory = ["./", "./style"];
-const sourceDirectory = ["./"];
-const destinationDirectory = "dist/";
+const sourceDirectory = ['./'];
+const destinationDirectory = 'dist/';
 
-sourceDirectory.forEach(dir =>{
-    cpyFile(dir,destinationDirectory)
-})
-
+sourceDirectory.forEach((dir) => {
+  cpyFile(dir, destinationDirectory);
+});
 
 function cpyFile(directory, destination) {
   fs.readdir(directory, (err, files) => {
@@ -20,19 +19,15 @@ function cpyFile(directory, destination) {
     }
 
     files.forEach((file) => {
-      if (path.extname(file) === ".html" || path.extname(file) === ".css") {
+      if (path.extname(file) === '.html' || path.extname(file) === '.css') {
         const sourceFile = path.join(directory, file);
         let destinationFile = path.join(destination, directory);
-
         fs.mkdir(destinationFile, { recursive: true }, (err) => {
-            if (err) throw err;
-          });
-
+          if (err) throw err;
+        });
         destinationFile = path.join(destinationFile, file);
-
         const readStream = fs.createReadStream(sourceFile);
         const writeStream = fs.createWriteStream(destinationFile);
-
         readStream.pipe(writeStream);
       }
     });
@@ -40,4 +35,4 @@ function cpyFile(directory, destination) {
 }
 
 //compile Ts file
-execSync("npx tsc");
+execSync('npx tsc');
